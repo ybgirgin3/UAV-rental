b11ac1a41e249a5ca70f92f52d01a792b1b11cbd
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 08:05:02 +0300

    user model

--- user/models.py
+++ user/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+
+class User(models.Model):
+    firstname = models.CharField(max_length=100)
+    middlename = models.CharField(max_length=100, blank=True, null=True, default='')
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField()
+    phone = models.CharField(max_length=12)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return f'{self.firstname} {self.middlename} {self.lastname}'
