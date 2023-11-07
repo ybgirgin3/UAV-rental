c5133dc425d1cccea7b0012546d1275eb1433edf
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 13:48:16 +0300

    custom serializers

--- rental/serializers.py
+++ rental/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers, status, response
-from .models import UAV, Customer, Reservation, Category
+from .models import UAV, Reservation, Category
+# from django.contrib.auth.models import User
+from user.models import User
+
 
 class UAVSerializer(serializers.ModelSerializer):
     """
@@ -19,19 +22,21 @@ class AvailableUAVSerializer(serializers.ModelSerializer):
         model = UAV
         fields = '__all__'
 
-class CustomerSerializer(serializers.ModelSerializer):
-    """
-        Serializer for Customer
-    """
-    class Meta:
-        model = Customer
-        fields = '__all__'
+# class CustomerSerializer(serializers.ModelSerializer):
+#     """
+#         Serializer for Customer
+#     """
+#     class Meta:
+#         model = Customer
+#         fields = '__all__'
 
 
 class ReservationSerializer(serializers.ModelSerializer):
     """
         Serializer for Reservation
     """
+    customer = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    uav = serializers.PrimaryKeyRelatedField(queryset=UAV.objects.filter(is_available=True))
     class Meta:
         model = Reservation
         fields = '__all__'
