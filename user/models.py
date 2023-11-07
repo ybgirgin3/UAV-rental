3d1ce34116bb0177ceb0b67635550651af4a678c
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 17:04:42 +0300

    model and profile created

--- user/models.py
+++ user/models.py
@@ -1,14 +1,38 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    firstname = models.CharField(max_length=100)
-    middlename = models.CharField(max_length=100, blank=True, null=True, default='')
-    lastname = models.CharField(max_length=100)
+# class User(models.Model):
+class User(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
     phone = models.CharField(max_length=12)
     is_active = models.BooleanField(default=True)
+
+    def profile(self):
+        profile = Profile.objects.get(user=self)
+
+    def __str__(self) -> str:
+        return f'{self.username}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=1000)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='user_images', default='default.jpg')
+    verified = models.BooleanField(default=False)
     
     def __str__(self) -> str:
-        return f'{self.firstname} {self.middlename} {self.lastname}'
+        return self.full_name
+    
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
--- user/views.py
+++ user/views.py
@@ -1,43 +1,33 @@
-from django.contrib.auth import authenticate
-from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
 
-from .serializers import UserSerializer
-from .models import User
+User = get_user_model()
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token/',
+        '/api/register/',
+        '/api/token/refresh/'
+    ]
+    return Response(routes)
 
 
-class UserRegistration(APIView):
-    permission_classes = [AllowAny]
-    
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UserLogin(APIView):
-    permission_classes = [AllowAny]
-    
-    def post(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
-        user = authenticate(
-            email=email,
-            password=password
-        )
-        if user:
-            refresh = RefreshToken.for_user(user)
-            token = {
-                'refresh': str(refresh),
-                'access': str(refresh.access_token)
-            }
-            return Response(token, status=status.HTTP_200_OK)
-        return Response({
-            'detail': "Invalid credentials"
-            }, status=status.HTTP_401_UNAUTHORIZED)
-            
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def 
