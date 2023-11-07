ddd05882a6c775a66453dd66f9f649cfa34224db
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 13:48:57 +0300

    admin

--- rental/admin.py
+++ rental/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import UAV, Reservation, Customer
+from .models import UAV, Reservation
 
 # Register your models here.
 
 admin.site.register(UAV)
 # admin.site.register(Customer)
-# admin.site.register(Reservation)
+admin.site.register(Reservation)
