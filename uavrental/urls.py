3a65a4690f12d93a3dc6c388a6d2976f8c299273
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 08:04:57 +0300

    router for refresh token

--- uavrental/urls.py
+++ uavrental/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 # viewsets
 from rental.views import UAVViewSet, CustomerViewSet, ReservationViewSet
@@ -13,5 +15,8 @@ router.register(r'reservation', ReservationViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
