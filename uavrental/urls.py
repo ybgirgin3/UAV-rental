ed164e187afc840246394ee2be5cea877607097a
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 13:48:42 +0300

    urls added for all modelviews

--- uavrental/urls.py
+++ uavrental/urls.py
@@ -5,13 +5,17 @@ from rest_framework import routers
 
 
 # viewsets
-from rental.views import UAVViewSet, CategoryViewSet
+from rental.views import UAVViewSet, CategoryViewSet, ReservationViewSet
+# from user.views import UserProfileView#, RegistrationView
+# from user.urls import urlpatterns as user_urlpatterns
 
 router = routers.DefaultRouter()
 router.register(r'uav', UAVViewSet)
 router.register(r'category', CategoryViewSet)
-# router.register(r'customer', CustomerViewSet)
-# router.register(r'reservation', ReservationViewSet)
+# router.register(r'customer', UserProfileView)
+# router.register(r'create-customer', RegistrationView)
+router.register(r'reservation', ReservationViewSet)
+
 
 urlpatterns = [
     # admin panel
@@ -19,10 +23,17 @@ urlpatterns = [
 
     ## MAIN API 
     path('api/', include(router.urls)),
+
+    # path('user', include('user.urls')),
     
 
     # authentication
     path('api-auth/', include('rest_framework.urls')),
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # path('auth/', include('user.urls')),
+
 ]
+
+# urlpatterns += user_urlpatterns
