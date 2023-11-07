568ad0dd8214108a3f8ccde099cd6f3002590272
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 22:38:41 +0300

    move files to backend folder

--- rental/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UAV, Reservation, Category
-
-# Register your models here.
-
-admin.site.register(UAV)
-admin.site.register(Category)
-admin.site.register(Reservation)
