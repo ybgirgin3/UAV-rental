3292ffa987e2bc95aeb64e29af286aa0006f3414
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 13:48:11 +0300

    fields created

--- rental/models.py
+++ rental/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+# from django.contrib.auth.models import User
+from user.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -23,17 +24,17 @@ class UAV(models.Model):
         return f'{self.name}: {self.description}'
 
 
-class Customer(models.Model):
-    """
-        Customer Model
-    """
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField()
+# class Customer(models.Model):
+#     """
+#         Customer Model
+#     """
+#     firstname = models.CharField(max_length=50)
+#     lastname = models.CharField(max_length=50)
+#     phone = models.CharField(max_length=20)
+#     email = models.EmailField()
 
-    def __str__(self) -> str:
-        return f'{self.firstname} {self.lastname}'
+#     def __str__(self) -> str:
+#         return f'{self.firstname} {self.lastname}'
 
 
 class Reservation(models.Model):
@@ -41,9 +42,10 @@ class Reservation(models.Model):
         Reservation Model
     """
     uav = models.ForeignKey(UAV, on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     issue_date = models.DateField()
     return_date = models.DateField()
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
-        return f'{self.uav}: {self.customer}, expires at: {self.end_time}'
+        return f'{self.uav}: {self.customer}, expires at: {self.return_date}'
