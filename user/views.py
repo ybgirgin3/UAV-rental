ccbb1203ea1d3c7cdcbba816e18505c96160ff38
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 08:05:20 +0300

    views for user registration and login

--- user/views.py
+++ user/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserRegistration(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(
+            email=email,
+            password=password
+        )
+        if user:
+            refresh = RefreshToken.for_user(user)
+            token = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+            return Response(token, status=status.HTTP_200_OK)
+        return Response({
+            'detail': "Invalid credentials"
+            }, status=status.HTTP_401_UNAUTHORIZED)
+            
