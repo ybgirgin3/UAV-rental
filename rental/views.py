71ba730f3fa6fe495da8fb6b15d4ceeda9493ac4
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 13:48:24 +0300

    custom views created for CRUD

--- rental/views.py
+++ rental/views.py
@@ -2,10 +2,9 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import UAV, Customer, Reservation, Category
+from .models import UAV, Reservation, Category
 from .serializers import (
     UAVSerializer,
-    CustomerSerializer,
     ReservationSerializer,
     CategorySerializer
 )
@@ -54,3 +53,69 @@ class UAVViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            # get uav and make it unavailable
+            uav = UAV.objects.get(pk=serializer.data.get('uav'))
+            uav.is_available = False
+            uav.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            # Custom logic for partial data validation and updating
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        print('instance', instance)
+        if instance:
+            try:
+                uav = UAV.objects.get(id=instance.uav.id)
+                # print('uav in delete', uav)
+                uav.is_available = True
+                uav.save()
+
+                instance.is_deleted = True
+                instance.save()
+
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            # raise Exception.NotFound('Object not found')
+            print('not found')
