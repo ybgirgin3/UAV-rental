3843b1d63998fdb25a54372c86722ff8e9bfbf37
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-07 08:16:24 +0300

    length added to user model

--- user/models.py
+++ user/models.py
@@ -5,8 +5,8 @@ class User(models.Model):
     firstname = models.CharField(max_length=100)
     middlename = models.CharField(max_length=100, blank=True, null=True, default='')
     lastname = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField()
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
     phone = models.CharField(max_length=12)
     is_active = models.BooleanField(default=True)
     
