db96c0180b1ac2ca15c2852d9aef7b8316fe91d6
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-09 00:12:46 +0300

    factory created

--- frontend/src/Components/TableFactory.js
+++ frontend/src/Components/TableFactory.js
@@ -3,27 +3,35 @@ import './TableFactory.css';
 
 const TableFactory = ({ data }) => {
   const columns = data.length > 0 ? Object.keys(data[0]) : [];
+  console.log('data in tablefactory', data);
+  console.log('column in tablefactory', columns);
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((column, index) => (
-            <th key={index}>{column}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, rowIndex) => (
-          <tr key={rowIndex}>
-            {columns.map((column, columnIndex) => (
-              <td key={columnIndex}>{item[column]}</td>
+  if (data.length > 0) {
+    return (
+      <div>
+        <table class="table-auto">
+          <thead>
+            <tr>
+              {columns.map((column, index) => (
+                <th key={index}>{column}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, rowIndex) => (
+              <tr key={rowIndex}>
+                {columns.map((column, columnIndex) => (
+                  <td key={columnIndex}>{item[column]}</td>
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return <div>No Data Found</div>;
+  }
 };
 
 export default TableFactory;
