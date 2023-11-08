4caa0045ad088ec4a690b5ef772b0a0bb4ce1652
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-11-08 23:09:55 +0300

    token paths

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,9 +3,17 @@ from . import views
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_protect
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView
+)
+
+
 
 urlpatterns = [
-    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('logout/', csrf_protect(views.LogoutView.as_view()), name ='logout')
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -4,30 +4,40 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
 
 User = get_user_model()
 
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+# class MyTokenObtainPairView(TokenObtainPairView):
+#     serializer_class = MyTokenObtainPairSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/token/',
-        '/api/register/',
-        '/api/token/refresh/'
-    ]
-    return Response(routes)
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 # @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-# def 
+# def getRoutes(request):
+#     routes = [
+#         '/api/token/',
+#         '/api/register/',
+#         '/api/token/refresh/'
+#     ]
+#     return Response(routes)
